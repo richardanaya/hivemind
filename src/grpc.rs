9b1837c7bf71aa154a3562c637f8866b87e6dc8f
Richard <Richard>
2020-07-18 12:46:39 -0700

    breaking up

---
+++ src/grpc.rs
@@ -0,0 +1,79 @@
+
+use tonic::{transport::Server, Request, Response, Status};
+use hivemind::hivemind_client::*;
+use hivemind::hivemind_server::*;
+use hivemind::*;
+
+pub mod hivemind {
+    tonic::include_proto!("hivemind");
+}
+
+
+#[derive(Debug, Default)]
+pub struct HivemindNode {}
+
+#[tonic::async_trait]
+impl Hivemind for HivemindNode {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().name).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    async fn get_key_value(
+        &self,
+        _request: Request<GetKeyValueRequest>,
+    ) -> Result<Response<GetKeyValueReply>, Status> {
+        Ok(Response::new(GetKeyValueReply {
+            message: "foo".into(),
+        }))
+    }
+
+    async fn set_key_value(
+        &self,
+        _request: Request<SetKeyValueRequest>,
+    ) -> Result<Response<SetKeyValueReply>, Status> {
+        Ok(Response::new(SetKeyValueReply {
+            message: "foo".into(),
+        }))
+    }
+}
+
+
+pub async fn start_server(){
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = HivemindNode::default();
+    Server::builder()
+        .add_service(HivemindServer::new(greeter))
+        .serve(addr)
+        .await
+        .unwrap();
+}
+
+pub async fn create_client() -> HivemindNodeClient {
+    let c = HivemindClient::connect("http://[::1]:50051").await.unwrap();
+    HivemindNodeClient {
+        grpc_client:c,
+    }
+}
+
+pub struct HivemindNodeClient{
+    grpc_client:HivemindClient<tonic::transport::channel::Channel>
+}
+
+impl HivemindNodeClient {
+    pub async fn say_hello(&mut self, t:&str) -> String {
+        let request = tonic::Request::new(hivemind::HelloRequest {
+            name: t.to_string(),
+        });
+        
+        let response = self.grpc_client.say_hello(request).await.unwrap();
+        response.get_ref().message.clone()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,11 @@
-use clap::derive::Clap;
-use hello_world::hivemind_client::*;
-use hello_world::hivemind_server::*;
-use hello_world::*;
-use tonic::{transport::Server, Request, Response, Status};
-
 mod cli;
+mod grpc;
+mod web;
 
-pub mod hello_world {
-    tonic::include_proto!("hivemind");
-}
+use clap::derive::Clap;
+use grpc::*;
+use web::start_web_server;
 
-#[derive(Debug, Default)]
-pub struct HivemindNode {}
-
-#[tonic::async_trait]
-impl Hivemind for HivemindNode {
-    async fn say_hello(
-        &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request: {:?}", request);
-
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
-        };
-
-        Ok(Response::new(reply))
-    }
-
-    async fn get_key_value(
-        &self,
-        _request: Request<GetKeyValueRequest>,
-    ) -> Result<Response<GetKeyValueReply>, Status> {
-        Ok(Response::new(hello_world::GetKeyValueReply {
-            message: "foo".into(),
-        }))
-    }
-
-    async fn set_key_value(
-        &self,
-        _request: Request<SetKeyValueRequest>,
-    ) -> Result<Response<SetKeyValueReply>, Status> {
-        Ok(Response::new(hello_world::SetKeyValueReply {
-            message: "foo".into(),
-        }))
-    }
-}
-
-use std::convert::Infallible;
-
-use hyper::service::{make_service_fn, service_fn};
-
-async fn hello(_: hyper::Request<hyper::Body>) -> Result<hyper::Response<hyper::Body>, Infallible> {
-    Ok(hyper::Response::new(hyper::Body::from("Hello World!")))
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -76,38 +28,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         ),
     }
 
-    let server = tokio::task::spawn(async {
-        let addr = "[::1]:50051".parse().unwrap();
-        let greeter = HivemindNode::default();
-        Server::builder()
-            .add_service(HivemindServer::new(greeter))
-            .serve(addr)
-            .await
-            .unwrap();
-    });
+    let server = tokio::task::spawn(grpc::start_server());
 
     let client = tokio::task::spawn(async {
+        let mut client = create_client().await;
         loop {
-            let mut client = HivemindClient::connect("http://[::1]:50051").await.unwrap();
-
-            let request = tonic::Request::new(HelloRequest {
-                name: "Tonic".into(),
-            });
-
-            let response = client.say_hello(request).await.unwrap();
-
-            println!("RESPONSE={:?}", response);
+            let response = client.say_hello("foo").await;
+            println!("{}", response);
             std::thread::sleep(std::time::Duration::from_secs(1));
         }
     });
 
-    let http_server = tokio::task::spawn(async {
-        let addr = ([127, 0, 0, 1], 3000).into();
-        let server = hyper::Server::bind(&addr).serve(make_service_fn(|_conn| async {
-            Ok::<_, Infallible>(service_fn(hello))
-        }));
-        server.await.unwrap();
-    });
+    let http_server = tokio::task::spawn(start_web_server());
 
     server.await?;
     client.await?;
